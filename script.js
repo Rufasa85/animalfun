d009eb39418685047fdad69f8b69dc57b547a03e
lindsfitz <lindsaytfitz@gmail.com>
2022-07-06 12:13:01 -0600

    random joke fetch added to js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("linked!")
+var setupDiv = document.getElementById('setup');
+var punchlineDiv = document.getElementById('punchline');
+var jokeButton = document.getElementById('joke-btn');
+
+function getApi() {
+  var requestUrl = 'https://manateejokesapi.herokuapp.com/manatees/random';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      setupDiv.textContent = data.setup;
+      punchlineDiv.textContent = data.punchline;
+    });
+}
+
+getApi();
+
+jokeButton.addEventListener('click', getApi);
